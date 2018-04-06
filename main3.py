6a01355c65033dbe2a4ebf1fcd8b7063630bc479
qubo93@gmail.com <5247594119a>
2018-04-06 18:19:12 +0800

    add default city

---
+++ main3.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+import urllib.parse
+import urllib.request
+import json
+import sys
+
+if  len(sys.argv)== 1:
+    location = input('what is your location?')
+    if location == "":
+        location = '北京'
+else:
+    location = sys.argv[1]
+r_location = urllib.parse.quote(location)
+link = 'http://api.map.baidu.com/telematics/v3/weather?location=' + r_location +'&output=json&ak=KPGX6sBfBZvz8NlDN5mXDNBF&callback='
+req = urllib.request.Request(link)
+with urllib.request.urlopen(req) as response:
+   the_page = response.read()
+#print (the_page)
+s = json.loads(the_page);
+print("今日各项指数：")
+for i in range(1,5):
+    print(s["results"][0]["index"][i]["title"])
+    print(s["results"][0]["index"][i]["zs"])
+    print(s["results"][0]["index"][i]["tipt"])
+    print(s["results"][0]["index"][i]["des"])
+    print("\n")
+
+for b in range(0,4):
+    print (s["results"][0]["currentCity"])
+    print (s["results"][0]["weather_data"][b]["date"]+ '天气')
+    print (s["results"][0]["weather_data"][b]["weather"])
+    print (s["results"][0]["weather_data"][b]["wind"])
+    print (s["results"][0]["weather_data"][b]["temperature"])
+    print("\n")
