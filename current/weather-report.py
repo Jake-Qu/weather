4a6411fc350d2ca9a62f023bbdf353a42564fe20
609_100005240305_qubo93 <609_100005240305_qubo93@git.cloud.tencent.com>
2018-08-02 16:07:29 +0800

    精简代码 加一些捕获异常

--- current/weather-report.py
+++ current/weather-report.py
@@ -9,9 +9,8 @@ def get_locatoin():
     urls = 'http://members.3322.org/dyndns/getip'
     req = urllib.request.Request(urls)
     with urllib.request.urlopen(req) as response:
-        ip = response.read()
-        r_ip = ip.decode('utf-8')
-    urls = 'http://freeapi.ipip.net/' + r_ip
+        ip = response.read().decode('utf-8')
+    urls = 'http://freeapi.ipip.net/' + ip
     req = urllib.request.Request(urls)
     with urllib.request.urlopen(req) as response:
         local = response.read()
@@ -22,7 +21,6 @@ def get_locatoin():
 if  len(sys.argv)== 1:
     location = input('what is your location?')
     if location == "":
-        print("请稍等，正在获取您的地理位置...")
         location = get_locatoin()
 else:
     location = sys.argv[1]
@@ -31,28 +29,31 @@ else:
 
 
 #取得天气
-def get_weather(locations='北京'):
-    r_location = urllib.parse.quote(locations)
+def get_weather(location):
+
+    r_location = urllib.parse.quote(location)
     link = 'http://api.map.baidu.com/telematics/v3/weather?location=' + r_location +'&output=json&ak=KPGX6sBfBZvz8NlDN5mXDNBF&callback='
     req = urllib.request.Request(link)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
-    s = json.loads(the_page);
-    print("今日各项指数：")
+    s = json.loads(the_page)
+    print("您的地点是：",location +".\n"+ "今日各项指数：")
+
     for i in range(1,5):
-        print(s["results"][0]["index"][i]["title"])
-        print(s["results"][0]["index"][i]["zs"])
-        print(s["results"][0]["index"][i]["tipt"])
-        print(s["results"][0]["index"][i]["des"])
+        for k in ['title','zs','tipt','des']:
+            try:
+                print(s["results"][0]["index"][i][k])
+            except KeyError:
+                print("Sorry, we can't currently check the weather outside of China.")
+                exit(1)
         print("\n")
 
+    print (s["results"][0]["currentCity"])
     for b in range(0,4):
-        print (s["results"][0]["currentCity"])
-        print (s["results"][0]["weather_data"][b]["date"]+ '天气')
-        print (s["results"][0]["weather_data"][b]["weather"])
-        print (s["results"][0]["weather_data"][b]["wind"])
-        print (s["results"][0]["weather_data"][b]["temperature"])
+        print (s["results"][0]["weather_data"][b]["date"]+ '天气:')
+        for k in ['weather','wind','temperature']:
+            print (s["results"][0]["weather_data"][b][k])
         print("\n")
+
 #赋值传参
-locations = location
-get_weather(locations)
+get_weather(location)
